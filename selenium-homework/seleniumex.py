3b9ab27bb946c73a19718b161137d6636ac9db9f
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-06-27 09:44:12 +0200

    1

---
+++ selenium-homework/seleniumex.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://index.hu")
+try:
+    q = driver.find_elements("nem létező")
+except:
+    print("hiba történt az elem keresése közben")
+    driver.close()
