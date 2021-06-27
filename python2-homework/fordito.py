61051fcc546417e96762d688557677030e241cf8
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-06-27 15:00:57 +0200

    1

--- python2-homework/fordito.py
+++ python2-homework/fordito.py
@@ -1,8 +1,8 @@
 szamok = []
 while True:
     szam = input("adj meg egy számot: ")
-    szamok.append(szam)
     if szam == "0":
         break
-for cv in range(len(szamok)):
-    print(szamok[len(szamok) - (cv + 1)])
+    szamok.append(szam)
+
+print(szamok[::-1])
