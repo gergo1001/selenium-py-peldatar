eea75b8f70daa84e38ae3ecf1c716650bb739f59
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-06-27 20:56:22 +0200

    1

--- selenium-homework/seleniumex.py
+++ selenium-homework/seleniumex.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
 
 
-def find_elements_ex(driver):
+def find_elements_ex(driver, elem):
     try:
-        q = driver.find_elements("nem létező")
+        q = driver.find_elements(elem)
     except:
-        print("hiba történt az elem keresése közben")
+        print(f"hiba történt a(z) <{elem}> keresése közben")
         driver.close()
 
 
 driver = webdriver.Chrome()
-find_elements_ex(driver)
+find_elements_ex(driver, "nem létező")
 
 # driver.get("https://index.hu")
 # try:
