c24d89ef9cf069e4d9c9d3b2cd65968f1b630589
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-06-27 13:01:48 +0200

    1

---
+++ selenium-homework/locators.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://localhost:9999/kitchensink.html")
+
+def attrubutomok(elem):
+    print("value (attr): ", elem.get_attribute("value"))
+    print("checked (prop): ", elem.get_property("checked"))
+    print("name: (prop)", elem.get_property("name"))  # nem tudom, hogy mire szolgál a cars, csak tipp
+    print("text: ", elem.text)
+    print("--------")
+
+
+#radiobutton
+bmw=driver.find_element_by_id("bmwradio")
+attrubutomok(bmw)
+
+combobox=driver.find_element_by_xpath("/html/body/div[2]/div[2]/fieldset/select")
+attrubutomok(combobox)
+
+multiselect=driver.find_element_by_name("multiple-select-example");
+attrubutomok(multiselect)
+
+#driver.close();
