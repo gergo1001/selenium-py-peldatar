6e850426735817b8a493c7bd1472c8a46e5472b1
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-06-28 13:02:31 +0200

    1

--- python-homework/consumption.py
+++ python-homework/consumption.py
@@ -1,9 +1,10 @@
 # (országúti fogyasztás, városi fogyasztás, országúton megtett út, városban megtett út, benzin ára)
 
-fogy_orsz = float(input("Kérem az országúti fogyasztást"))
-fogy_var = float(input("Kérem a városi fogyasztást"))
-ut_orsz = float(input("Kérem az országúti távot"))
-ut_var = float(input("Kérem a városi távot"))
-benzin_ar = int(input("Kérem a benzin árát"))
+fogy_orsz = float(input("Kérem az országúti fogyasztás: "))
+fogy_var = float(input("Kérem a városi fogyasztást: "))
+ut_orsz = float(input("Kérem az országúti távot: "))
+ut_var = float(input("Kérem a városi távot: "))
+benzin_ar = int(input("Kérem a benzin árát: "))
 
-print((ut_orsz / 100 * fogy_orsz * benzin_ar) + (ut_var / 100 * fogy_var * benzin_ar))
+koltseg = (ut_orsz / 100 * fogy_orsz * benzin_ar) + (ut_var / 100 * fogy_var * benzin_ar)
+print(f"össz költség: {koltseg}")
