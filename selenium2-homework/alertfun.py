f2d6a663fbecf881c75b969e6470de68cede01d1
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-07-12 18:08:08 +0200

    félig kész

---
+++ selenium2-homework/alertfun.py
@@ -0,0 +1,48 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('http://localhost:9999/alert_playground.html')
+
+button = driver.find_element_by_name('alert')
+button.click()
+alert = driver.switch_to.alert
+time.sleep(3)
+alert.accept()
+
+
+
+button = driver.find_element_by_name("confirmation")
+button.click()
+alert = driver.switch_to.alert
+time.sleep(3)
+alert.accept()
+
+button.click()
+time.sleep(3)
+alert.dismiss()
+
+button=driver.find_element_by_id("double-click")
+action = ActionChains(driver)
+action.double_click(button).perform()
+
+text_now=driver.find_element_by_id("demo").text
+button=driver.find_element_by_name("prompt")
+button.click()
+alert=driver.switch_to.alert
+send_text="123456"
+alert.send_keys(send_text)
+alert.dismiss()
+assert (driver.find_element_by_id("demo").text,text_now)
+alert=driver.switch_to.alert
+send_text="123456"
+alert.send_keys(send_text)
+alert.accept()
+assert (driver.find_element_by_id("demo").text,f"You entered: {send_text}")
+
